7536b2cf4dc5b48bddd0b27daffe818c4fd0f9f1
Mark Bensley <bensleym25@gmail.com>
2019-09-11 12:04:28 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+// import SearchParams from "./SearchParams";
+// import { Router, Link } from "@reach/router";
+// import Details from "./Details";
+// import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  return <h1>Hello World</h1>;
+};
+
+render(<App />, document.getElementById("root"));
