f738342acb94ef9dcfbd4d669439c820a9536a42
Mark Bensley <bensleym25@gmail.com>
2019-10-30 18:02:11 +0000

    Mobile navigation fix

--- src/js/navigation/Navigation.js
+++ src/js/navigation/Navigation.js
@@ -18,19 +18,49 @@ class Navigation extends Component {
               <span className="fas fa-times"></span>
             </button>
           </li>
-          <Link to="/why-choose-us" className="navigation__link">
+          <Link
+            to="/why-choose-us"
+            className="navigation__link"
+            onClick={() => {
+              MenuControl.menuClosed();
+            }}
+          >
             <li className="navigation__item">Why Choose Us</li>
           </Link>
-          <Link to="/where-we-can-help" className="navigation__link">
+          <Link
+            to="/where-we-can-help"
+            className="navigation__link"
+            onClick={() => {
+              MenuControl.menuClosed();
+            }}
+          >
             <li className="navigation__item">Where We Can Help</li>
           </Link>
-          <Link to="/get-an-offer" className="navigation__link">
+          <Link
+            to="/get-an-offer"
+            className="navigation__link"
+            onClick={() => {
+              MenuControl.menuClosed();
+            }}
+          >
             <li className="navigation__item">Get An Offer</li>
           </Link>
-          <Link to="/standards-and-ethics" className="navigation__link">
+          <Link
+            to="/standards-and-ethics"
+            className="navigation__link"
+            onClick={() => {
+              MenuControl.menuClosed();
+            }}
+          >
             <li className="navigation__item">Standards &amp; Ethics</li>
           </Link>
-          <Link to="/contact-us" className="navigation__link">
+          <Link
+            to="/contact-us"
+            className="navigation__link"
+            onClick={() => {
+              MenuControl.menuClosed();
+            }}
+          >
             <li className="navigation__item">Contact Us</li>
           </Link>
         </ul>
